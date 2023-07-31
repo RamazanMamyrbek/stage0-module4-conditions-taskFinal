e45670efad49e6d067f532a4dfdd4acbcce1c4ab
RamazanMamyrbek <rama.mamirbek@gmail.com>
2023-07-31 18:12:56 +0600

    s

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        first = first ^ second; // XOR to get the intermediate result
+        second = first ^ second; // XOR with second to get the original value of first
+        first = first ^ second;// XOR with first to get the original value of second
+
+        System.out.println(first);
+        System.out.println(second);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,41 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year < 0 || month < 1 || month > 12) {
+            System.out.println("invalid date");
+            return;
+        }
 
+        int daysInMonth = 0;
+
+        // Check if the year is a leap year
+        boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+        // Calculate the number of days in the given month
+        switch (month) {
+            case 1: // January
+            case 3: // March
+            case 5: // May
+            case 7: // July
+            case 8: // August
+            case 10: // October
+            case 12: // December
+                daysInMonth = 31;
+                break;
+            case 4: // April
+            case 6: // June
+            case 9: // September
+            case 11: // November
+                daysInMonth = 30;
+                break;
+            case 2: // February
+                daysInMonth = isLeapYear ? 29 : 28;
+                break;
+            default:
+                System.out.println("invalid date");
+                return;
+        }
+
+        System.out.println(daysInMonth);
     }
 }
